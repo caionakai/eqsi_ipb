5973d96b44727838e25a2d8d6f14667823bbd151
Igor Faustino <igornfaustino@gmail.com>
2019-12-20 18:42:27 +0000

    add navbar

--- src/admin/index.js
+++ src/admin/index.js
@@ -1,21 +1,25 @@
 import React from "react";
+import { Main } from "grommet";
 import { BrowserRouter as Switch, Route } from "react-router-dom";
 
 import RegisterCompanies from "./pages/RegisterCompanies";
 import RegisterEmployees from "./pages/RegisterEmployees";
+import CustomHeader from "../common/components/Header";
 
 export default function Admin() {
   return (
     <>
-      HEADER
-      <Switch>
-        <Route path="/admin/companies/register">
-          <RegisterCompanies />
-        </Route>
-        <Route path="/admin/employees/register">
-          <RegisterEmployees />
-        </Route>
-      </Switch>
+      <CustomHeader />
+      <Main pad="large" align="center">
+        <Switch>
+          <Route path="/admin/companies/register">
+            <RegisterCompanies />
+          </Route>
+          <Route path="/admin/employees/register">
+            <RegisterEmployees />
+          </Route>
+        </Switch>
+      </Main>
     </>
   );
 }
