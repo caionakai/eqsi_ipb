dca610bfd7c08792512ec4b1b84e1c0ec4ec1755
Igor Faustino <igornfaustino@gmail.com>
2019-12-20 18:19:44 +0000

    add react router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Admin from "./admin";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Route path="/admin">
+          <Admin />
+        </Route>
+      </Router>
+    </>
   );
 }
-
-export default App;
---
+++ src/admin/pages/RegisterCompanies.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RegisterCompanies() {
+    return (
+        <div>
+            Register Companies
+        </div>
+    )
+}
+
+export default RegisterCompanies
